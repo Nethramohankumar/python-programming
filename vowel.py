06b83ccf308fbc12c84023ff027d6c12e209f968
Nethramohankumar <46022951+Nethramohankumar@users.noreply.github.com>
2019-05-30 09:40:44 +0530

    Create vowel.py

---
+++ vowel.py
@@ -0,0 +1,8 @@
+vlist=['a','e','i','o','u']
+clist=['b','c','d','e','f','g','h','j','k','l','m'.'n','p','q','r','s','t','v','w','x','y''z']
+if(x in vlist):
+print("vowel")
+elif(x in clist):
+print("Consonant")
+else:
+print("invalid")
